4f4e9f284758aae416adbcc433f6d0d681a99fc0
Jeremy Sullivan <suddenlysullivan@gmail.com>
2016-07-20 21:13:07 +0000

    init commit

---
+++ preview.sh
@@ -0,0 +1,3 @@
+#because I can't get frog preview to bind on 0.0.0.0
+#make sure `raco` is on your $PATH and frog is installed, and python is installed and on $PATH
+raco frog -b && python -m SimpleHTTPServer;
